5fa558e2ef3de636dbde2a028f2b5d13a8983f1a
John Doe <w0rframeac@gmail.com>
2026-05-22 19:45:51 -0700

    Made an item

--- src/main/java/com/epicmod/Epicmod.java
+++ src/main/java/com/epicmod/Epicmod.java
@@ -1,5 +1,6 @@
 package com.epicmod;
 
+import com.epicmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -11,6 +12,6 @@ public class Epicmod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
--- src/main/java/com/epicmod/EpicmodDataGenerator.java
+++ src/main/java/com/epicmod/EpicmodDataGenerator.java
@@ -1,11 +1,14 @@
 package com.epicmod;
 
+import com.epicmod.datagen.ModModelProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
 public class EpicmodDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		var pack = fabricDataGenerator.createPack();
 
+		pack.addProvider(ModModelProvider::new);
 	}
 }
