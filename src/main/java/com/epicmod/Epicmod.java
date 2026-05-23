3c55af7c500e54f7045243ee62b709c074669351
John Doe <w0rframeac@gmail.com>
2026-05-22 19:00:05 -0700

    Initial commit

---
+++ src/main/java/com/epicmod/Epicmod.java
@@ -0,0 +1,16 @@
+package com.epicmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Epicmod implements ModInitializer {
+	public static final String MOD_ID = "epicmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
